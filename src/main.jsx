1df7c5081e07caac1ca433a78e193d1557d28d17
Victoria Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2023-09-24 02:53:09 +0300

    add fixes to the router

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter basename="/wheels4rent">
+        <HashRouter>
           <GlobalStyle />
           <App />
         </HashRouter>
