b9d605146d9808a014b7bd205df83a2438f8d4da
Victoria Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2023-09-25 22:47:19 +0300

    fix app routs

--- src/main.jsx
+++ src/main.jsx
@@ -13,11 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter
-          basename={
-            import.meta.env.DEV ? '/' : '/react-vite-project-template-js/'
-          }
-        >
+        <BrowserRouter basename={import.meta.env.DEV ? '/' : '/wheels4rent/'}>
           <GlobalStyle />
           <App />
         </BrowserRouter>
